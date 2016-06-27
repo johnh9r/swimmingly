c2456fbac8ff36cd1812babcc683b367d9ac862c
John H <johnh9r@example.org>
2016-06-27 22:56:06 +0100

    stub out functionality of Save button
    see JavaScript console for JSON that could be sent to backend server

--- app/routes/index.js
+++ app/routes/index.js
@@ -8,5 +8,21 @@ export default Ember.Route.extend({
             mainSet: [],
             cooldownSet: []
         };
+    },
+
+    actions: {
+        save() {
+            let session = {
+                venue: Ember.$('input#venue')[0].value,
+                date: Ember.$('input#date')[0].value,
+                lane: Ember.$('input#lane')[0].value,
+                coach: Ember.$('input#coach')[0].value
+            };
+            console.log(JSON.stringify(session));
+            console.log(JSON.stringify(this.controller.model.warmupSet));
+            console.log(JSON.stringify(this.controller.model.mainSet));
+            console.log(JSON.stringify(this.controller.model.cooldownSet));
+            return false;  // do not bubble further
+        }
     }
 });
